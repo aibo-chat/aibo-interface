d6f78e06c769ba2b257a29e630c32eb799eab3b3
Porianesu <976384615@qq.com>
2024-03-26 16:17:19 +0800

    Update: Add baisc ui of Aibo-interface v0.4

--- src/util/jsonComputed.ts
+++ src/util/jsonComputed.ts
@@ -1,7 +1,7 @@
 import AiboJson from '../../public/res/json/aibo.json'
 import anyImage from '../../public/res/json/images/img_2.png?url'
 
-const finalPath = anyImage.replace(/img_2.png/, '')
+const finalPath = anyImage.replace(/\/(?:[^\/]*)$/, '/')
 
 AiboJson.assets.forEach((asset) => {
   asset.u = finalPath
